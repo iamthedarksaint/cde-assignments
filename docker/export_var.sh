d9d1cf5b0e7a96a28814db3285e9c4bf256aa08d
iamthedarksaint <bojzino128@gmail.com>
2025-09-21 15:10:34 +0100

    removed

--- docker/export_var.sh
+++
@@ -1,6 +0,0 @@
-export DB_NAME=parch_and_posey
-export DB_USER=postgres
-export DB_PASSWORD=postgres
-export DB_PORT=5432
-export DB_HOST=127.0.0.1
-export URL="https://www.stats.govt.nz/assets/Uploads/Annual-enterprise-survey/Annual-enterprise-survey-2023-financial-year-provisional/Download-data/annual-enterprise-survey-2023-financial-year-provisional.csv"
